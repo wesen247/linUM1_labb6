a0f6df9c15aec664dedb0ce29cd881bd2293d37d
wesen247 <z.wesen@protonmail.com>
2021-03-10 13:51:01 +0100

    initiated libpower class and header file

---
+++ lib/libpower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+#include "libpower.h"
+
+float calc_power_r(float volt, float resistance)
+{
+	return 0;
+}
+
+float calc_power_i(float volt, float current)
+{
+	return 0;
+}
